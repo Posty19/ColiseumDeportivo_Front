f0de45a337479af17b6e25fef09590dcf680ec39
Daniel Postigo <danielpostigo96@gmail.com>
2025-05-29 15:59:15 +0200

    amliado el comentario de organizacion

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,24 @@ function App() {
       home
       articulos => coments
       personajes
+      contacto
       signIn/Cuenta
       dashboard => protegidas
         dashboard home
         CRUD articulos
         CRUD personajes
+    
+    layouts:
+      home
+      dasboard
+      login? => layout o ternario para ocultar nav/footer
+
+    components:
+      formulario
+      card
+      content => pensar nombre mejor
+        notable?
+        article?
 
     estado global:
       datos de user => cargar al logIn
